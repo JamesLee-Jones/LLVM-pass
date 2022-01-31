cabe6943373870bba44837ac58a22691d51f60ba
James Lee-Jones <jl8320@ic.ac.uk>
2022-01-31 01:54:38 +0000

    Implemented basic functionality for counting the number of instructions in a block

--- Count/Count.cpp
+++ Count/Count.cpp
@@ -1,3 +1,6 @@
+#include <map>
+#include <iterator>
+
 #include "llvm/Pass.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Module.h"
@@ -17,16 +20,24 @@ namespace {
 		virtual bool runOnModule(Module &M) override {
 			long num_functions = 0;
 			long num_basic_blocks = 0;
+			std::map<int,int> instr_in_block;
 
 			for(auto &F : M) {
 				num_functions++;
-				errs().write_escaped(F.getName()) << "\n";
-				for(auto &B : F)
+				for(auto &B : F) {
 					num_basic_blocks++;
+					int num_instr = 0;
+					for(auto &I : B)
+						num_instr++;
+					instr_in_block[num_instr]++;
+				}
 			}
 
 			errs() << "The program has a total of " << num_functions <<
 				" functions and " << num_basic_blocks << " basic blocks.\n";
+			for(auto const& [key, value] : instr_in_block) {
+				errs() << key << ": " << value << "\n";
+			}
 
 			return false;
 		}
